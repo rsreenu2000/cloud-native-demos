7dd3ff276bdff3f6d065a4dc45872dd7e5f60cd4
Lu Ken <ken.lu@intel.com>
2020-02-14 09:52:21 +0800

    refine the gen-k8s-yaml script to handle the special characters like : / in prefix(namespace) of registry name

--- elastic_inference/tools/gen-k8s-yaml.sh
+++ elastic_inference/tools/gen-k8s-yaml.sh
@@ -31,15 +31,23 @@ function process_args {
 
 process_args "$@"
 
+if [ -z "$TEMPLATE_FILE_PATH" ]; then
+    echo "Please specify the template file via -f."
+    usage
+fi
+
 TEMPLATE_FILE_NAME=$(basename ${TEMPLATE_FILE_PATH})
 TEMPLATE_DIR=$(dirname ${TEMPLATE_FILE_PATH})
-YAML_FILE_NAME=`echo "$TEMPLATE_FILE_NAME" | sed 's@.yaml.template@'_"$REGISTRY".yaml'@'`
+# REGISTRY prefix(namespace) may containers characters ":", "/", so need replace
+# these characters to "_" for generated yaml filename
+REGISTRY_TO_PATH=`echo "$REGISTRY" | sed 's/[:/]/_/g'`
+YAML_FILE_NAME=`echo "$TEMPLATE_FILE_NAME" | sed 's@.yaml.template@'_"$REGISTRY_TO_PATH".yaml'@'`
 
 echo "Registry: ${REGISTRY}"
 echo "Template: ${TEMPLATE_FILE_NAME}"
 echo "Yaml file: ${YAML_FILE_NAME}"
 
 cp ${TEMPLATE_FILE_PATH} ${TEMPLATE_DIR}/${YAML_FILE_NAME}
-sed -i -e 's/your-own-registry/'"$REGISTRY"'/g' ${TEMPLATE_DIR}/${YAML_FILE_NAME}
+sed -i -e "s%your-own-registry%$REGISTRY%g" ${TEMPLATE_DIR}/${YAML_FILE_NAME}
 
 echo "Generate kubernete yaml file @${YAML_FILE_NAME} with registry ${REGISTRY}"
